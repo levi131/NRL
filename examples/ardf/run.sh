be903f79ffa80c8077569d6320b3c53026078501
levi131 <2275534138@qq.com>
2025-10-14 09:38:01 +0000

    update readme

--- examples/ardf/run.sh
+++ examples/ardf/run.sh
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Package initializer for examples."""
+python3 nrl/entry.py examples/ardf/config.py
